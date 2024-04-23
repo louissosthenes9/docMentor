72c891cd679a22745a09d9ce5936f4e2fa70b421
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-23 19:33:05 +0300

    new layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +13,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='light'>
+      <body className={cn(
+        'min-h-screen font-sans antialiased'
+      )}>
+        {children}
+      </body>
     </html>
   )
 }
