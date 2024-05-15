b78d809646e779d029eaf364a3df0a1c4da02990
louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 16:24:29 +0300

    fixed trpc error

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -2,6 +2,7 @@
 import React, { Children, PropsWithChildren, useState } from 'react'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import { trpc } from '@/app/_trpc/client';
+import { httpBatchLink } from '@trpc/client';
 
 export default function Provider({children} :PropsWithChildren) {
     const [queryClient] = useState(()=>new QueryClient())
