487a737c81dac0c63329af11488bab9f999c7c00
louissosthenes9 <louissosthenes9@gmail.com>
2024-05-20 20:56:25 +0300

    fixed the kinde auth bug

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,27 @@
-import {router} from './trpc'
-
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import {publicProcedure, router} from './trpc'
+import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
+import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 export const appRouter = router({
+    authCallback:publicProcedure.query(
+       async ()=>{
+            const {getUser} = getKindeServerSession()
+            const user = await getUser() as KindeUser
 
+            if(!user.id || !user.email) throw new TRPCError({code:"UNAUTHORIZED"})
+     
+          //check if the user is in the database
+           const dbUser = await db.user.findFirst({
+            where:{
+                id:user.id
+            }
+           })
+          
+          return {success:true}
+        }
+    )
+  
 })
 
 export type AppRouter = typeof appRouter;
