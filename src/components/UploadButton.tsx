46e4b170bfa6210a953dcabe3531ca0a690e559b
louissosthenes9 <louissosthenes9@gmail.com>
2024-06-01 00:22:09 +0300

    custom dialog button

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,9 +1,25 @@
 "use client"
+import { DialogContent, DialogTrigger } from "@radix-ui/react-dialog"
+import { Dialog } from "./ui/dialog"
 import { useState } from "react"
+import { Button } from "./ui/button"
 
 export default function UploadButton() {
    const [isOpen, setIsOpen] = useState(false)
   return (
-    <div>UploadButton</div>
+    <Dialog open = {isOpen} onOpenChange={(v)=>{
+      if(!v){
+        setIsOpen(v)
+      }
+    }}>
+  <DialogTrigger asChild  onClick={()=>{setIsOpen(true)}}> 
+    <Button>Upload pdf</Button>
+   </DialogTrigger>
+
+
+   <DialogContent>
+         
+   </DialogContent>
+    </Dialog>
   )
 }
