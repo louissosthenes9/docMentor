e3760895d08d633243a1d52d73fa6d7e3911733a
louissosthenes9 <louissosthenes9@gmail.com>
2024-06-02 01:58:05 +0300

    added a middleware

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,25 @@
-import {initTRPC} from "@trpc/server"
+import {initTRPC, TRPCError} from "@trpc/server"
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import {KindeUser} from "@kinde-oss/kinde-auth-nextjs/types";
 
 const t = initTRPC.create();
+const middleware = t.middleware
+const isAuth = middleware(async(opts)=>{
+    const {getUser} = getKindeServerSession()
+    const user =  await getUser() as KindeUser
 
+    if(!user){
+        throw new TRPCError({code: 'UNAUTHORIZED'})
+    }
+
+    return opts.next({
+        ctx:{
+            userId:user.id,
+            user,
+            name:'john'
+        }
+    })
+})
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuth);
