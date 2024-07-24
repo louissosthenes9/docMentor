0fe281b7af4c358eec5fd5cf05b21ccab088c1b1
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-24 12:58:42 +0300

    initial pdf renderer

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,13 @@
-
+ 
 export default function PdfRenderer() {
   return (
-    <div>PdfRenderer</div>
+     <div className="w-full rounded-md flex flex-col bg-white shadow items-center">
+         <div className="h-14 px-2 w-full border-b border-zinc-200 flex items-center justify-between">
+             
+                <div className="flex items-center gap-1 5">
+                    top bar
+                </div>
+         </div>
+     </div>
   )
 }
