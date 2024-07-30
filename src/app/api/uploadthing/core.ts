c76e7082ce2dd1d5273cb937ecbe658e35810ace
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-30 22:40:53 +0300

    uploadthing config

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,33 +1,35 @@
+
+import { db } from "@/db";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import { userAgent } from "next/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
  
 const f = createUploadthing();
  
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
- 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
+ 
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
- 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
+    
+      const {user} = useKindeBrowserClient()
+
+      if(!user || user.id) throw new Error('Unauthorized')
+      return {userId:user.id};
+    }) 
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
- 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
+        const createdFile = await db.file.create({
+          data:{
+            key:file.key,
+            name:file.name,
+            userId:metadata.userId,
+            url:`https://uploadthing-prod.s3.us-west.amazonaws.com/${file.key}`,
+            uploadStatus:'PROCESSING', 
+          }
+        })
+      
+      return {};
     }),
 } satisfies FileRouter;
  
