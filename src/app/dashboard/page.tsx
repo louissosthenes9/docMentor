ead0488a2408c16739729e0ad4b6fe2d7ef243c8
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-31 11:02:32 +0300

    revert to serverside components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,27 @@
-"use client"
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect} from 'next/navigation'
 import { trpc } from '@/app/_trpc/client';
 import Dashboard from '@/components/Dashboard';
 import { db } from "@/db";
-const page = () => {
-   const {user} =useKindeBrowserClient();
+const page = async () => {
+   const {getUser} =getKindeServerSession()
+   const user = await getUser();
+   console.log(user)
+
    
    //check if the user is authenticated
-  //if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
   //sync to the db
- // const dbUser = await db.user.findFirst({
-//  where: {
-  //    id: user.id
-  //  }
-  //})
+  const dbUser = await db.user.findFirst({
+  where: {
+      id: user.id
+    }
+  })
 
   //if not in db then redirect
-  //if(!dbUser) redirect('/auth-callback?origin=dashboard')
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
   
   return (
       <Dashboard />
