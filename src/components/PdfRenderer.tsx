a67fabf6116fdfc05ad1df3dee4c5ecc8c3266d1
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-31 11:03:48 +0300

    fixed layout issue

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -4,7 +4,7 @@ export default function PdfRenderer() {
      <div className="w-full rounded-md flex flex-col bg-white shadow items-center">
          <div className="h-14 px-2 w-full border-b border-zinc-200 flex items-center justify-between">
              
-                <div className="flex items-center gap-1 5">
+                <div className="flex items-center gap-1.5">
                     top bar
                 </div>
          </div>
