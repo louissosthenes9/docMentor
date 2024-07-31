28a60fd2d2bbf38ab28fd36f614366c3bc0cd9f6
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-31 11:02:11 +0300

    added loading states and proper error handling

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,35 +1,43 @@
-"use client"
-import { useRouter, useSearchParams } from 'next/navigation'
-import React from 'react'
-import { trpc } from '../_trpc/client'
-import { Loader2 } from 'lucide-react'
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
+
 
 export default function page() {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
 
-    const {data, isLoading}= trpc.authCallback.useQuery(undefined,{
-      onSuccess:({success})=>{
-        router.push(origin?`${origin}`:'/dashboard')
-      },
-      onError:(error)=>{
-        if(error.data?.code==="UNAUTHORIZED"){
-          router.push("/sign-in")
-        }
-      },
-      retry:true,
-      retryDelay:500
-    })
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      router.push("/dashboard");
+    },
+    onError: (error) => {
+      if (error.data?.code === "UNAUTHORIZED") {
+        router.push("/sign-in");
+      }
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="w-full flex justify-center mt-24">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+      </div>
+    );
+  }
+
   return (
-     <div className="w-full flex justify-center  mt-24">
-       <div className='flex flex-col items-center gap-2'>
-          <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
-          <h3 className='font-semibold text-xl'>setting up your account...</h3>
-          <p>You will be directed automatically</p>
-        
-       </div>
-
-     </div>
-  )
+    <div className="w-full flex justify-center  mt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+        <h3 className="font-semibold text-xl">setting up your account...</h3>
+        <p>You will be directed automatically</p>
+      </div>
+    </div>
+  );
 }
