1c37d5179e12d9170d9c4246c8fd6cd78c22b519
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-06 10:07:36 +0300

    added pdf dist

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config) => {
     config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
     return config;
   },
 };
