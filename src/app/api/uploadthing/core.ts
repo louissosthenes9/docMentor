61dee1c722900dee322dfb818be19c6c1e35ef0e
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-14 13:12:47 +0300

    fixed openai import error

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,10 +1,14 @@
-
+import { OpenAIEmbeddings } from '@langchain/openai'
 import { db } from "@/db";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import { userAgent } from "next/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
- 
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf'
+import { Pinecone } from "@pinecone-database/pinecone";
+import pc from "@/lib/pinecone";
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+
 const f = createUploadthing();
  
 export const ourFileRouter = {
@@ -29,6 +33,34 @@ export const ourFileRouter = {
             uploadStatus:'PROCESSING', 
           }
         })
+
+        try {
+          
+          const response = await fetch(file.url)
+
+          const blob = await response.blob()
+
+          const loader = new PDFLoader(blob)
+
+          const pageLevelDocs = await loader.load()
+
+          const pagesAnt = pageLevelDocs.length 
+
+
+          //vectorizatiob
+          const pineconeIndex = pc.Index("doc")
+
+          const embeddings = new OpenAIEmbeddings({
+            apiKey:process.env.OPEN_AI_KEY!
+          })
+
+          await PineconeStore
+
+
+
+        } catch (error) {
+          
+        }
       
       return {};
     }),
