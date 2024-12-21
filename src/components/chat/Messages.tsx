95fe5f91c36e950b5a8e14b615fb0a188fd019d8
louissosthenes9 <louissosthenes9@gmail.com>
2024-12-21 19:52:47 +0300

    added skeletons

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,23 +1,76 @@
-import { trpc } from '@/app/_trpc/client'
-import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
-import React from 'react'
+import { trpc } from '@/app/_trpc/client';
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
+import { Loader2, MessagesSquare } from 'lucide-react';
+import Skeleton from 'react-loading-skeleton'
+import Message from './Message';
 
-type MessagesProps ={
-  fileId:string
-}
-const Messages = ({fileId}:MessagesProps) => {
-  const {} = trpc.getFileMessages.useInfiniteQuery({
-    fileId,
-    limit:INFINITE_QUERY_LIMIT
-  },{
-    getNextPageParam:(lastPage)=>lastPage?.nextCursor,
-    keepPreviousData:true
-  })
+type MessagesProps = {
+  fileId: string;
+};
+
+const Messages = ({ fileId }: MessagesProps) => {
+  const { data, isLoading, fetchNextPage } = trpc.getFileMessages.useInfiniteQuery(
+    {
+      fileId,
+      limit: INFINITE_QUERY_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+      keepPreviousData: true,
+    }
+  );
+
+  const messages = data?.pages.flatMap((page) => page.messages);
+  const loadingMessage = {
+    createdAt: new Date().toISOString(), 
+    id: 'loading-message',
+    isUserMessage: false,
+    text: (
+      <span className="flex items-center justify-center">
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </span>
+    ),
+  };
+
+  const combinedMessages = [
+    ...(isLoading ? [loadingMessage] : []), 
+    ...(messages ?? []),
+  ];
 
   return (
-    <div className='flex max-h-[calc(100vh-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-2 overflow-y-auto scrollbar-thumb-blue scrolbar-thumb-rounded scrollbar-track-lighter scrollbar-w-2 scrolling-touch'>
+    <div className="flex max-h-[calc(100vh-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-2 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-lighter scrollbar-w-2 scrolling-touch">
+      {(combinedMessages && (combinedMessages.length > 0 ))? (
+       
+        combinedMessages.map((message, i) => {
+          const isNextmessageSamePerson = combinedMessages[i-1]?.isUserMessage === combinedMessages[i]?.isUserMessage
+        
+        return  <Message
+          key={message.id}
+          message={message} 
+          isNextMessageSamePerson={isNextmessageSamePerson}           
+          />
+})
+      ) : isLoading ? (
+        <div className="w-full flex flex-col gap-2">
+            <Skeleton className = 'h-16' />
+            <Skeleton className = 'h-16' />
+            <Skeleton className = 'h-16' /> 
+            <Skeleton className = 'h-16' />   
+            
+        </div>
+      ) : (
+        <div className="flex-1 flex-col items-center justify-center gap-2">
+             <MessagesSquare className='h-8 w-8 text-blue-500' />
+              <h3 className="font-semibold text-xl">
+                 You&apos;re all set !
+              </h3>
+              <p className="text-zinc-500 text-sm">
+                Ask your first questions to get started
+              </p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
