d2c699eb8bb5ea830f185363ac3f2a9dbc8b3fdb
louissosthenes9 <louissosthenes9@gmail.com>
2025-01-07 17:50:03 +0300

    fixed types

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,9 +1,9 @@
- import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
- export async function GET(request:NextRequest ,{params}:any){
-   
-   const endpoint = params.kindeAuth;
+export async function GET(request: NextRequest, { params }: any): Promise<NextResponse> {
+  const endpoint = params.kindeAuth;
 
-   return handleAuth(request,endpoint)
- }
+  const response = await handleAuth(request, endpoint);
+  return NextResponse.json(response);
+}
