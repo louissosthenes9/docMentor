18d27cd2a94cb0549f935f9c4d74079abd2906c0
louissosthenes9 <louissosthenes9@gmail.com>
2025-02-02 02:47:25 +0300

    added types for syntax highlighter

--- src/react-syntax-highlighter.d.ts
+++ src/react-syntax-highlighter.d.ts
@@ -1,1 +1,6 @@
 declare module 'react-syntax-highlighter';
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
+    const content: { [key: string]: any };
+    export default content;
+}
