88fd264daa8020b418acd1a4724cbf47cf609eec
louissosthenes9 <louissosthenes9@gmail.com>
2025-04-10 22:52:03 +0300

    updates

---
+++ src/app/seo.config.js
@@ -0,0 +1,12 @@
+const SEO = {
+    title: 'DocMentor',
+    description: 'Chat with your documents in seconds',
+    openGraph: {
+      type: 'website',
+      locale: 'en_IE',
+      url: 'https://docmentor.onrender.com',
+      siteName: 'DocMentor',
+    },
+  };
+
+  export default SEO;
